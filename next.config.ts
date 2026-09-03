422e69708c3d0359bb2741cab76f2186b0ec898f
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2026-09-03 02:06:09 -0500

    Optimizations

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,18 @@ const exposeTestingApi = process.env.EXPOSE_TESTING_API === "1";
 const nextConfig: NextConfig = {
   agentRules: false,
   cacheComponents: true,
+  reactCompiler: true,
   partialPrefetching: true,
   poweredByHeader: false,
   reactStrictMode: true,
   typedRoutes: true,
+  compiler: {
+    styledComponents: true,
+  },
   experimental: {
     exposeTestingApiInProductionBuild: exposeTestingApi,
+    inlineCss: true,
+    turbopackRustReactCompiler: true,
   },
   redirects() {
     return Promise.resolve([
--- oxfmt.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "oxfmt";
-
-export default defineConfig({
-  printWidth: 80,
-  sortTailwindcss: true,
-});
